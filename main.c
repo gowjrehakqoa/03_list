758da012c91810627fb7bc3a78515b78059e70c3
gowjrehakqoa <42432599+gowjrehakqoa@users.noreply.github.com>
2019-10-08 02:17:40 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+#include <queue>
 
 int main(int argc, char *argv[]) 
 {
+    queue<int> q; 
     char str[5];
-    
+    int num , count =0;
     while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
+      q.push(1);
+      q.push(2);
+      q.push(3);
+      q.push(4);
+       q.push(5);
+        if(q.push == 0){
+            printf("r,\n");
+        }
+        else if(q.push == -1){
+            
+            
     }
 
     return 0;
